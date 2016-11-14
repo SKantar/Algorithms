bceee7d9bd2a27e262fcea6e8890317b980641bf
skantar <sladjan.kantar@netbox.co.rs>
2016-11-15 00:04:03 +0100

    void push(Node **head, int data) Given a reference (pointer to pointer) to the head of a list and an int,  inserts a new node on the front of the list. void append(Node **head, int data) Given a reference (pointer to pointer) to the head of a list and an int, appends a new node at the end void insert(Node **head, int data) Given a reference (pointer to pointer) to the head of a list and an int, insert sorted into list void remove(Node **head, int key) Given a reference (pointer to pointer) to the head of a list and a key, deletes the first occurrence of key in linked list int count(Node *head) Counts no. of nodes in linked list int recursive_count(Node *node) Counts no. of nodes in linked list int search(Node *head, int x) Checks whether the value x is present in linked list int recursive_search(Node *head, int x) Checks whether the value x is present in linked list void reverse(Node **head) Function to reverse the linked list void clear(Node **node) Function to delete the entire linked list void remove_duplicates(Node *head) The function removes duplicates from a sorted list void print(Node *head) This function prints contents of linked list starting from head

--- linked_list.c
+++ linked_list.c
@@ -1,76 +1,232 @@
-//
-// Created by skantar on 14/11/16.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct Node {
-    int value;
+
+typedef struct Node{
+    int data;
     struct Node *next;
 }Node;
 
-void print(Node *node){
-    while(node != NULL){
-        printf("%d ", node->value);
-        node = node->next;
-    }
+/* Given a reference (pointer to pointer) to the head of a list
+   and an int,  inserts a new node on the front of the list. */
+void push(Node **head, int data){
+    Node *node = (Node*)malloc(sizeof(data));
+    node->data = data;
+    node->next = *head;
+
+    *head = node;
 }
 
-void insert_at_the_beginning(Node **node, int value){
-    Node *new_node = (Node*)malloc(sizeof(Node));
-    new_node->value = value;
-    new_node->next = *node;
+/* Given a reference (pointer to pointer) to the head
+   of a list and an int, appends a new node at the end  */
+void append(Node **head, int data) {
+    Node *current = *head;
 
-    *node = new_node;
-}
+    Node *node = (Node*)malloc(sizeof(data));
+    node->data = data;
+    node->next = NULL;
 
-void insert_at_the_end(Node **node, int value) {
-    Node *new_node = (Node*)malloc(sizeof(Node));
-    new_node->value = value;
-    new_node->next = NULL;
 
-    if(*node == NULL){
-        *node = new_node;
+    if(current == NULL){
+        *head = node;
         return;
     }
 
-    Node *tmp_node = *node;
-
-    while(tmp_node->next != NULL)
-        tmp_node = tmp_node->next;
-
-    tmp_node->next = new_node;
-}
-
-void clear(Node **root){
-
-    Node *tmp_node = *root, *del;
-
-    while(tmp_node != NULL){
-        del = tmp_node;
-        tmp_node = tmp_node->next;
-        printf("\nDelete: %d", del->value);
-        free(del);
+    while(current->next != NULL){
+        current = current->next;
     }
 
-    *root = NULL;
+    current->next = node;
+
+}
+
+/* Given a reference (pointer to pointer) to the head
+   of a list and an int, insert sorted into list  */
+void insert(Node **head, int data) {
+
+    Node *current = *head, *prev;
+    Node *node = (Node*)malloc(sizeof(data));
+    node->data = data;
+
+    if (current == NULL || current->data > data){
+        node->next = current;
+        *head = node;
+        return;
+    }
+
+    prev = current;
+    current = current->next;
+
+    while(current != NULL){
+        if(current->data >= data){
+            node->next = current;
+            prev->next = node;
+            return;
+        }
+        prev = current;
+        current = current->next;
+    }
+
+    node->next = NULL;
+    prev->next = node;
+
+}
+
+/* Given a reference (pointer to pointer) to the head of a list
+   and a key, deletes the first occurrence of key in linked list */
+void remove(Node **head, int key){
+    Node *current = *head, *prev;
+
+    while(current->data == key){
+        *head = current->next;
+        free(current);
+    }
+
+    prev = current;
+    current = current->next;
+
+    while(current != NULL){
+        if(current->data == key){
+            prev->next = current->next;
+            free(current);
+        }
+        else
+            prev = current;
+        current = prev->next;
+    }
+
+}
+
+/* Counts no. of nodes in linked list */
+int count(Node *head){
+    int result = 0;
+    while(head != NULL){
+        head = head->next;
+        result++;
+    }
+    return result;
+}
+
+/* Counts no. of nodes in linked list */
+int recursive_count(Node *node){
+    if(node == NULL)
+        return 0;
+    return recursive_count(node->next) + 1;
+}
+
+/* Checks whether the value x is present in linked list */
+int search(Node *head, int x){
+    while(head != NULL){
+        if(head->data == x)
+            return 1;
+        head = head->next;
+    }
+    return 0;
+}
+
+/* Checks whether the value x is present in linked list */
+int recursive_search(Node *head, int x){
+    if(head == NULL)
+        return 0;
+    if(head->data == x)
+        return 1;
+    return recursive_search(head->next, x);
+}
+
+/* Function to reverse the linked list */
+void reverse(Node **head){
+    Node *prev = NULL, *current = *head, *next;
+    while(current != NULL){
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+}
+
+/* Function to delete the entire linked list */
+void clear(Node **node){
+    Node *next;
+
+    while ((*node)->next != NULL){
+        next = (*node)->next;
+        (*node)->data = next->data;
+        (*node)->next = next->next;
+        free(next);
+    }
+
+    free(*node);
+    *node = NULL;
+}
+
+/* The function removes duplicates from a sorted list */
+void remove_duplicates(Node *head){
+    if(head == NULL)
+        return;
+
+    Node *next = head->next;
+    while(next != NULL){
+        if(next->data == head->data) {
+            head->next = next->next;
+            free(next);
+        }
+        else
+            head = head->next;
+        next = head->next;
+    }
+}
+
+
+// This function prints contents of linked list starting from head
+void print(Node *head){
+    while(head != NULL){
+        printf("%d ", head->data);
+        head = head->next;
+    }
 }
 
 
 int main(){
+    Node *head = NULL;
 
-    Node *root = NULL;
-    insert_at_the_beginning(&root, 3);
-    insert_at_the_beginning(&root, 5);
-    insert_at_the_beginning(&root, 2);
-    insert_at_the_end(&root, 4);
-    insert_at_the_end(&root, 1);
-    insert_at_the_end(&root, 2);
-    insert_at_the_end(&root, 3);
+//    push(&head, 5);
+//    push(&head, 3);
+//    push(&head, 1);
+//
+//    append(&head, 2);
+//    append(&head, 4);
+//    append(&head, 6);
 
-    print(root);
-    clear(&root);
-    print(root);
+    insert(&head, 5);
+    insert(&head, 3);
+    insert(&head, 10);
+    insert(&head, 5);
+    insert(&head, 5);
 
-    return 0;
+    print(head);
+    printf("\n");
+    printf("%20s %d\n", "count iterative", count(head));
+    printf("%20s %d\n", "count recursive", recursive_count(head));
+    printf("%20s %d %s\n", "search iterative", 5,search(head, 5) ? "yes" : "no");
+    printf("%20s %d %s\n", "search iterative", 10,search(head, 10) ? "yes" : "no");
+    printf("%20s %d %s\n", "search iterative", 2,search(head, 2) ? "yes" : "no");
+    printf("%20s %d %s\n", "search recursive", 5,recursive_search(head, 5) ? "yes" : "no");
+    printf("%20s %d %s\n", "search recursive", 10,recursive_search(head, 10) ? "yes" : "no");
+    printf("%20s %d %s\n", "search recursive", 2,recursive_search(head, 2) ? "yes" : "no");
+
+    printf("\n");
+    reverse(&head);
+    print(head);
+
+    printf("\n");
+    remove_duplicates(head);
+    print(head);
+
+    printf("\n");
+    clear(&head);
+    print(head);
+
+
+
 }
